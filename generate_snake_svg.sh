15a25cd27375b09b9ef17a3cfc87ef89ca874788
Vinayak Yadav <168169129+vinayaky7@users.noreply.github.com>
2024-06-29 08:30:19 +0530

    Create generate_snake_svg.sh

---
+++ generate_snake_svg.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Generating GitHub contribution graph snake SVG..."
+# Replace this command with the actual command to generate the SVG file
+echo "<svg> ... </svg>" > ./dist/github-contribution-grid-snake.svg
